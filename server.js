ab6df1da965f98fb57077c5e279e0984ad95935d
Dallin parker <dallin.r.parker@icloud.com>
2019-03-16 13:00:39 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config()
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get('/test', (req, res, next) =>{
+	res.send('success')
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
